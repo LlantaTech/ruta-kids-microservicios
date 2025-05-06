1f9e5b4ae715988111576a4476d9258be3dc8466
Alex Avila <alex.ramon2004@gmail.com>
2025-05-06 17:53:35 -0500

    feat: done add open api config.

--- keycloak-service/src/main/java/org/pe/llantatech/keycloakservice/dto/LoginResponseDto.java
+++ keycloak-service/src/main/java/org/pe/llantatech/keycloakservice/dto/LoginResponseDto.java
@@ -1,5 +1,6 @@
 package org.pe.llantatech.keycloakservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 /**
@@ -8,10 +9,21 @@ import lombok.Data;
  * Date: 4/26/25 @ 16:45
  */
 @Data
+@Schema(description = "DTO representing the response after a successful login")
 public class LoginResponseDto {
+
+    @Schema(description = "Access token for the user", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
     private String access_token;
+
+    @Schema(description = "Refresh token for the user", example = "dGhpcyBpcyBhIHJlZnJlc2ggdG9rZW4...")
     private String refresh_token;
+
+    @Schema(description = "Time in seconds until the access token expires", example = "3600")
     private String expires_in;
+
+    @Schema(description = "Time in seconds until the refresh token expires", example = "7200")
     private String refresh_expires_in;
+
+    @Schema(description = "Type of the token issued", example = "Bearer")
     private String token_type;
 }
