238a745297556d855199d21594470f7156d76009
Ryzeon <alex.ramon2004@gmail.com>
2025-06-11 21:34:38 -0500

    Feat: Adds initial files service structure
    Adds the basic structure for the files service.
    
    This includes:
    - A `File` model.
    - A `FileService` interface.
    - `application.yaml` for configuration.
    - `application-docker.yml` for Docker-specific configuration.
    
    Removes the default `application.properties` file.

---
+++ files-service/src/main/java/org/pe/llantatech/files/model/File.java
@@ -0,0 +1,9 @@
+package org.pe.llantatech.files.model;
+
+/**
+ * Created by Alex Avila Asto - A.K.A (Ryzeon)
+ * Project: ruta-kids-microservicios
+ * Date: 6/11/25 @ 21:33
+ */
+public class File {
+}
---
+++ files-service/src/main/java/org/pe/llantatech/files/service/FileService.java
@@ -0,0 +1,11 @@
+package org.pe.llantatech.files.service;
+
+/**
+ * Created by Alex Avila Asto - A.K.A (Ryzeon)
+ * Project: ruta-kids-microservicios
+ * Date: 6/11/25 @ 21:33
+ */
+public interface FileService {
+
+
+}
